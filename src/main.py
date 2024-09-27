c241baeca18bfbe1f39b4f6a6c4698e938f5ad6b
David Sha <david.sha2002@gmail.com>
2024-09-27 14:52:08 +1000

    feat: create client and server classes

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from collections import deque
 from algorithms.x3dh import X3DH
 from algorithms.doublerachet import DoubleRachet
 
@@ -6,7 +7,33 @@ def main():
     Demonstrate X3DH and DoubleRachet algorithms in the context of the
     Signal Protocol.
     """
+    server = Server()
+    alice = Client('Alice')
+    bob = Client('Bob')
+    
+    # establish connection between alice and bob (x3dh)
+    
+
+    # send messages between alice and bob (double ratchet)
+
     return
+
+class KeyPair():
+    def __init__(self):
+        return
+
+class Client:
+    def __init__(self, name: str):
+        self.name = name
+        self.ik = KeyPair()
+        self.ek = KeyPair()
+        self.spk = KeyPair()
+        self.opk = KeyPair()
+
+class Server:
+    def __init__(self):
+        self.message_queue = deque([])
+
 
 if __name__ == '__main__':
     main()
