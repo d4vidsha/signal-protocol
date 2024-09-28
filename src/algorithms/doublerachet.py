4fd187e1f8e8dd005c6c9f2d931862c1398e959c
David Sha <david.sha2002@gmail.com>
2024-09-28 16:54:29 +1000

    feat: use linters and formatters

--- src/algorithms/doublerachet.py
+++ src/algorithms/doublerachet.py
@@ -1,3 +1,3 @@
-class DoubleRachet():
+class DoubleRachet:
     def __init__(self):
         return
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from collections import deque
 from algorithms.x3dh import X3DH
 from algorithms.doublerachet import DoubleRachet
+from enum import Enum
+
 
 def main():
     """
@@ -8,32 +10,44 @@ def main():
     Signal Protocol.
     """
     server = Server()
-    alice = Client('Alice')
-    bob = Client('Bob')
-    
+    alice = Client("Alice")
+    bob = Client("Bob")
+
     # establish connection between alice and bob (x3dh)
-    
 
     # send messages between alice and bob (double ratchet)
 
     return
 
-class KeyPair():
+
+class Curve(Enum):
+    X25519 = 0
+    X448 = 1
+
+
+class KeyPair:
     def __init__(self):
         return
 
+
 class Client:
-    def __init__(self, name: str):
+    def __init__(
+        self, name: str, curve: Curve = Curve.X25519, info: str = "MyProtocol"
+    ):
         self.name = name
+        self.info: str = info
+        self.curve: Curve
         self.ik = KeyPair()
         self.ek = KeyPair()
         self.spk = KeyPair()
         self.opk = KeyPair()
+        self.mk = KeyPair()
+
 
 class Server:
     def __init__(self):
         self.message_queue = deque([])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
